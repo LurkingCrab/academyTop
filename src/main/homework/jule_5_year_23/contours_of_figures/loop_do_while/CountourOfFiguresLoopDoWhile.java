324d4e5d4015e9bd2000f514a57a8524ddac037d
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2023-07-04 13:20:00 +0500

    Добавлены контурные фигуры, в пакет countourus_of_figure\loop_do_while/

--- src/main/homework/jule_5_year_23/contours_of_figures/loop_do_while/CountourOfFiguresLoopDoWhile.java
+++ src/main/homework/jule_5_year_23/contours_of_figures/loop_do_while/CountourOfFiguresLoopDoWhile.java
@@ -1,66 +1,10 @@
-package main.homework.jule_5_year_23.contours_of_figures.loop_while;
+package main.homework.jule_5_year_23.contours_of_figures.loop_do_while;
 
-public class ContoursOfFiguresLoopWhile {
+public class CountourOfFiguresLoopDoWhile {
     public static void main(String[] args) {
-        getCountSize();
-        System.out.println();
-
-//        контурный прямоугольный треугольник
-        int countLine = 1;
-        while (countLine <= 8) {
-            int countCharacter = 1;
-            while (countCharacter <= countLine) {
-                if (countLine == 8 || countCharacter == 1 || countCharacter == countLine) {
-                    System.out.print('*');
-                } else {
-                    System.out.print(' ');
-                }
-                countCharacter++;
-            }
-            System.out.println();
-            countLine++;
-        }
-        System.out.println();
-
-        //контурный перевернутый прямоугольный треугольник
-        int i = 10;
-        while (i >= 1) {
-            int j = 1;
-            while (j <= i) {
-                if (i == 10 || j == 1 || j == i) {
-                    System.out.print('*');
-                } else {
-                    System.out.print(' ');
-                }
-                j++;
-            }
-            System.out.println();
-            i--;
-        }
-
-        //      контурный квадрат
-        int figureSize111 = 8;
-        int countI = 0;
-        while (countI < figureSize111) {
-            int countJ = 0;
-            while (countJ < figureSize111) {
-                if (countJ == 0 || countJ == figureSize111 - 1 || countI == 0 || countI == figureSize111 - 1) {
-                    System.out.print('*');
-                } else {
-                    System.out.print(' ');
-                }
-                countJ++;
-            }
-            System.out.println();
-            countI++;
-        }
-
-    }
-
-    private static void getCountSize() {
         int figure = 10;
         int countSize = 0;
-        while (countSize <= figure) {
+        do{
             int countLine = 0;
             while (countLine < figure - countSize) {
                 System.out.print(" ");
@@ -77,8 +21,59 @@ public class ContoursOfFiguresLoopWhile {
             }
             System.out.println();
             countSize++;
-        }
+        }while (countSize <= figure);
 
+        System.out.println();
+
+        int countLine = 1;
+        do {
+            int countCharacter = 1;
+             do {
+                if (countLine == 8 || countCharacter == 1 || countCharacter == countLine) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+                countCharacter++;
+            }while (countCharacter <= countLine);
+            System.out.println();
+            countLine++;
+        }while (countLine <= 8);
+
+        System.out.println();
+
+        int i = 10;
+        do {
+            int j = 1;
+            while (j <= i) {
+                if (i == 10 || j == 1 || j == i) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+                j++;
+            }
+            System.out.println();
+            i--;
+        }while (i >= 1);
+
+        System.out.println();
+
+        int figureSize111 = 8;
+        int countI = 0;
+        do {
+            int countJ = 0;
+            while (countJ < figureSize111) {
+                if (countJ == 0 || countJ == figureSize111 - 1 || countI == 0 || countI == figureSize111 - 1) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+                countJ++;
+            }
+            System.out.println();
+            countI++;
+        }while (countI < figureSize111);
     }
 
 }
