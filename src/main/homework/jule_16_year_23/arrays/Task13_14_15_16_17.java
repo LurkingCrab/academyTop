f43139862410bfc56e768f2378cbf871c5091221
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2023-07-11 12:51:38 +0500

    Выполнены задач c 13 по 17

--- src/main/homework/jule_16_year_23/arrays/Task13_14_15_16_17.java
+++ src/main/homework/jule_16_year_23/arrays/Task13_14_15_16_17.java
@@ -4,22 +4,28 @@ package main.homework.jule_16_year_23.arrays;
 import static main.homework.jule_16_year_23.arrays.Task1And2.arr;
 
 //    protected static int[] arr = new int[]{10, 2, 233, 4, 185, -435, -5, 3990, -400, 5, 5397, 523, 5, -6, 7, 8, 9, -11};
-public class Task13And14And15 {
+public class Task13_14_15_16_17 {
 
 //     13)Вывести количество двузначных чисел
 //     14)Вывести количество трехзначных чисел
 //     15)Вывести количество однозначных чисел
+//     16)Вывести сумму всех однозначных четных чисел
+//     17)Вывести сумму всех двузначных четных чисел
 
     public static void main(String[] args) {
+        int sumOfSingleDigitElements = 0;
+        int sumOfTwoDigitElements = 0;
         int numberOfSingleDigitElements = 0;
         int numberOfTwoDigitElements = 0;
         int numberOfThreeDigitElements = 0;
         for (int element : arr) {
             if (element <= -1 && element >= -9 || element >= 0 && element <= 9) {
                 numberOfSingleDigitElements++;
+                sumOfSingleDigitElements+=element;
 //                System.out.println(element);
             } else if (element <= -10 && element >= -99 || element >= 10 && element <= 99) {
                 numberOfTwoDigitElements++;
+                sumOfTwoDigitElements += element;
 //                System.out.println(element);
             } else if (element <= -100 && element >= -999 || element >= 100 && element <= 999) {
                 numberOfThreeDigitElements++;
@@ -28,6 +34,7 @@ public class Task13And14And15 {
         }
         System.out.printf("Количество однозначных элементов массива - %d\nКоличество двузначных элементов массива - %d\n" +
                 "Количество трехзначных элементов массива - %d\n", numberOfSingleDigitElements, numberOfTwoDigitElements, numberOfThreeDigitElements);
+        System.out.printf("Сумма однозначных элементов массива - %d\nСумма двухзначных элементов массива - %d\n", sumOfSingleDigitElements, sumOfTwoDigitElements);
 
     }
 }
