dae2e6d33d9d34e020ef19c664232501a38589a0
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2023-07-12 14:01:29 +0500

    не значительные изменения

--- src/main/homework/jule_16_year_23/arrays/Task1And2.java
+++ src/main/homework/jule_16_year_23/arrays/Task1And2.java
@@ -3,7 +3,7 @@ package main.homework.jule_16_year_23.arrays;
 // 1)Вывести количество четных
 // 2)Вывести количество нечетных
 public class Task1And2 {
-    protected static int[] arr = new int[]{10, 2, 233, 4, 185, -435, -5, 3990, -400, 5, 5397, 523, 5, -6, 7, 8, 9, -11};
+    public static int[] arr = new int[]{10, 2, 233, 4, 185, -435, -5, 3990, -400, 5, 5397, 523, 5, -6, 7, 8, 9, -11};
 
     public static void main(String[] args) {
         int evenNumbers = 0;
