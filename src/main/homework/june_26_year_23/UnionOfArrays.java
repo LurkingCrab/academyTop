9d7256188cfb35997015c36f6df6eb65a3f4f8ae
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2023-07-20 15:52:12 +0500

    Слияние сортировка двух массивов

---
+++ src/main/homework/june_26_year_23/UnionOfArrays.java
@@ -0,0 +1,35 @@
+package main.homework.june_26_year_23;
+
+import java.util.Arrays;
+
+/**
+ * Функция которая, принимает 2 массива и возвращает новый массив. Объединяет 2 массива
+ * --Нельзя все собрать в 1 массив, а потом отсортировать
+ */
+public class UnionOfArrays {
+    private static final int[] arr = new int[]{1, 4, 5, 7, 8, 12, 14, 15, 15, 15, 15, 900};
+    private static final int[] arr2 = new int[]{-1, 0, 9, 9, 9, 9, 10, 15, 921};
+
+    public static void main(String[] args) {
+        System.out.printf(Arrays.toString(findNewArray(arr, arr2)));
+
+    }
+
+    public static int[] findNewArray(int[] arr, int[] arr2) {
+
+        int[] newArray = new int[arr.length + arr2.length];
+        int z = 0;
+        for (int i = 0, j = 0; i < arr.length || j < arr2.length; ) {
+
+            if (arr[i] > arr2[j]) {
+                newArray[z++] = arr2[j];
+                j++;
+            } else {
+                newArray[z++] = arr[i];
+                i++;
+            }
+
+        }
+        return newArray;
+    }
+}
